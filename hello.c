74c3f82cd140670c317716bffec761f7f39ea016
Nikhil Singh <ns968560@gmail.com>
2023-10-10 23:07:28 +0530

    add status code

--- hello.c
+++ hello.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int number = 5;
 
+    printf('HELLO GITHUB');
+
     for (int i = 1; i <= 10; i++)
     {
         printf("\n %d * %d = %d", number, i, (number * i));
     }
-    return 0;
+    return 1;
 }
